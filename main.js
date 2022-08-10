0d0e73b81aceae7e39b532fef63542357e796801
ZeeshanAliii <317Zeeali@gmail.com>
2022-08-10 17:59:33 +1200

    my-fristcommit

---
+++ main.js
@@ -0,0 +1,38 @@
+// variables
+let xjav = 23  
+xjav = "changed 123"
+alert(xjav)
+
+let japaneseOralSex = "Xjav Premium"
+alert(japaneseOralSex)
+
+
+// after using this command you cannot change the 123 again
+const cantbechanged = 123
+
+// adds into console log for f12
+const inputElement = document.querySelector(".username")
+
+
+let javx = null
+let aoishirosaki = undefined
+let BBCshrosaki = false
+let shrosakirepairman = true 
+aoishirosaki = "creampienobabies"
+
+
+console.log(inputElement)
+console.log(aoishirosaki)
+
+
+
+// functions
+shiroskaiclub("hello, ", "asukachann")
+
+function shiroskaiclub(fristnumber,secondnumber){
+let total= fristnumber+secondnumber
+console.log(total)
+}
+
+
+// + addition * times /divison -minus
